d895345993d95a3ddff4029e260a3f68f972db39
mcarena <mcarena.quijada@outlook.com>
2024-03-06 19:17:31 -0300

    Se incorpora el metodo foreach con los 20 id y sus correspondientes titulos

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,18 @@ const fetchData = async () => {
       // PUNTO 3 FUNCION FETCH RECIBE EL VALOR DE LA PROMESA
       const response = await fetch(url);
       const data = await response.json();
-
+  
+      //PUNTO 4 METODO FOREACH QUE TRAE LOS 20 TITULOS
+      data.slice(0, 20).forEach(album => {
+        //MUESTRA EL ID 
+        console.log(album.id);
+        //MUESTRA EL TITULO
+        console.log(album.title);
+      });
     } catch (error) {
       console.error('Error al obtener los datos:', error);
     }
   };
-  
-
+ 
   // LLAMA A LA FUNCION PRINCIPAL ASINCRONICA
   fetchData();
